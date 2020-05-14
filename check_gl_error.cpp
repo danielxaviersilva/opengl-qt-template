ea0af845642193da0fa5ece4ea58d06fc32c19b5
Daniel Xavier <danielxaviers@gmail.com>
2020-05-13 21:53:03 -0300

    same commit as before, but now with the new files added into it

---
+++ check_gl_error.cpp
@@ -0,0 +1,19 @@
+void QCheckingOpenGL::_check_gl_error(const char *file, int line) {
+    //Ting: sugestao para chamada
+    //_check_gl_error(__FILE__,__LINE__)
+    GLenum err (glGetError());
+    while(err!=GL_NO_ERROR) {
+        std::string error;
+        switch(err) {
+            case GL_INVALID_OPERATION: error="INVALID_OPERATION"; break;
+            case GL_INVALID_ENUM: error="INVALID_ENUM"; break;
+            case GL_INVALID_VALUE: error="INVALID_VALUE"; break;
+            case GL_OUT_OF_MEMORY: error="OUT_OF_MEMORY"; break;
+            case GL_INVALID_FRAMEBUFFER_OPERATION: error="INVALID_FRAMEBUFFER_OPERATION"; break;
+            case GL_STACK_UNDERFLOW: error="GL_STACK_UNDERFLOW"; break;
+            case GL_STACK_OVERFLOW: error="GL_STACK_OVERFLOW"; break;
+        }
+        std::cout << "GL_" << error.c_str() <<" - "<< file << ":" << line << std::endl;
+        err=glGetError();
+    }
+}
